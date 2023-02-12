4686d383d38d4387c15533d302da9cdc6dedb54a
imperator1440@gmail.com <imperator1440@gmail.com>
2023-02-12 19:38:18 +0300

    authentication complete with observer pattern

--- src/components/sign-in/sign-in-form.component.jsx
+++ src/components/sign-in/sign-in-form.component.jsx
@@ -28,7 +28,7 @@ const SignInForm = () => {
     event.preventDefault(); 
 
     try {
-      const response = await signInAuthUserWithEmailAndPassword(email, password);
+      const { user } = await signInAuthUserWithEmailAndPassword(email, password);
       resetFormFields();
     } catch (error) {
       switch (error.code) {
@@ -46,13 +46,11 @@ const SignInForm = () => {
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-
     setFormFields({ ...formFields, [name]: value });
   };
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user);
+    await signInWithGooglePopup();
   };
 
   return (
