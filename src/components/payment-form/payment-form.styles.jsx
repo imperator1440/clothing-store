e5fa7104ba4edcc5a63c37d51cd65dd5a80eba52
imperator1440@gmail.com <imperator1440@gmail.com>
2023-02-26 21:03:34 +0300

    payment system with stripe.js

---
+++ src/components/payment-form/payment-form.styles.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const PaymentFormContainer = styled.div`
+  height: 300px;
+`
+
+export const FormContainer = styled.div`
+  height: 150px;
+  min-width: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
